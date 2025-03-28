795bd6347d3eccef20b61b0ff5f422bffb830354
TeddyPoon2 <tatlampoon@gmail.com>
2025-03-28 21:12:16 +0800

    add new task

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,35 +1,62 @@
-import React from "react";
-import { View, Text, FlatList, StyleSheet } from "react-native";
-import data from "./data.json";
-
-// const HomeScreen = () => {
-//   return (
-//     <View style={styles.container}>
-//       <Text style={styles.title}>Welcome to the Home Screen!</Text>
-//     </View>
-//   );
-// };
-
-// const showList = (items) => {
-//   return items.map((item) => {
-//     const date = new Date(item.createdAt);
-//     const timestamp =
-//       date.toLocaleDateString() + " " + date.toLocaleTimeString();
-
-//     return [
-//       <View key={item._id} style={styles.itemContainer}>
-//         <Text style={styles.title}>{item.title}</Text>,
-//         <Text style={styles.description}>{item.description}</Text>,
-//         <Text>
-//           By: {item.user} - Comments: {item.commentCount}
-//         </Text>
-//         ,<Text style={styles.meta}>{timestamp}</Text>,
-//       </View>,
-//     ];
-//   });
-// };
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  ActivityIndicator,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { API_URL } from "@env";
 
 const HomeScreen = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${API_URL}/tasks`);
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleSubmit = async () => {
+    if (title === "" || description === "") {
+      alert("Fill in something");
+      return;
+    }
+    
+    try {
+      const res = await fetch(`${API_URL}/tasks`, {
+        method: "POST",
+        header: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ title, description }),
+      });
+      // if (res.ok) {
+        const newTask = await res.json();
+        setData((prevData) => [...prevData, newTask]);
+        setTitle("");
+        setDescription("");
+      // }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const showList = ({ item }) => {
     const date = new Date(item.createdAt);
     const timestamp =
@@ -47,6 +74,14 @@ const HomeScreen = () => {
     ];
   };
 
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Todo List (Map)</Text>
@@ -55,6 +90,21 @@ const HomeScreen = () => {
         renderItem={showList}
         keyExtractor={(item) => item._id}
       />
+      <TextInput
+        style={styles.input}
+        placeholder="Title"
+        value={title}
+        onChangeText={setTitle}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Description"
+        value={description}
+        onChangeText={setDescription}
+      />
+      <TouchableOpacity onPress={handleSubmit}>
+        <Text style={styles.btn}>Sumbit</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -91,6 +141,16 @@ const styles = StyleSheet.create({
   meta: {
     fontSize: 12,
     color: "#666",
+  },
+  btn: {
+    fontSize: 18,
+    color: "white",
+    marginTop: 20,
+    padding: 15,
+    backgroundColor: "red",
+    borderColor: "grey",
+    borderWidth: 2,
+    borderRadius: 5,
   },
 });
 
